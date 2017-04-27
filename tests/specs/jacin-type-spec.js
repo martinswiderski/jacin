742045f64962982af4bddbe90162a0f199f07763
Martin Swiderski <martin.swiderski@gmail.com>
2017-04-27 12:29:45 +0100

    type

--- tests/specs/jacin-type-spec.js
+++ tests/specs/jacin-type-spec.js
@@ -1,7 +1,7 @@
 require('./../../src/tool/test-signature')(__filename);
 
 var jacin = require('./../../index'),
-    obj = jacin({});
+    varType = jacin({}).type();
 
 function CustomObject() {
 
@@ -9,11 +9,12 @@ function CustomObject() {
 
 describe('reads types', function () {
     it('basic ctypes', function () {
-        expect(obj.type().is(123)).toBe('number.int');
-        expect(obj.type().is(2.71828)).toBe('number.float');
-        expect(obj.type().is(6.022E23)).toBe('number.scientific');
-        expect(obj.type().is({})).toBe('object');
-        expect(obj.type().is(new CustomObject())).toBe('object.CustomObject');
-        expect(obj.type().is([])).toBe('array');
+        expect(varType.is(123)).toBe('number.int');
+        expect(varType.is(2.71828)).toBe('number.float');
+        expect(varType.is(6.022E23)).toBe('number.scientific');
+        expect(varType.is({})).toBe('object');
+        expect(varType.is(new CustomObject())).toBe('object.CustomObject');
+        expect(varType.is([])).toBe('array');
+        expect(varType.is(new Array())).toBe('array');
     });
 });
