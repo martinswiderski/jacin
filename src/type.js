c14c7c8c3eba48c8ae973eb2ae2fe22e7182d60c
Martin Swiderski <martin.swiderski@gmail.com>
2017-04-27 12:19:26 +0100

    types

--- src/type.js
+++ src/type.js
@@ -10,7 +10,8 @@
  */
 
 
-var _validTypes = {
+var _t = null,
+    _validTypes = {
         'null': true,
         boolean: true,
         'undefined': true,
@@ -22,39 +23,48 @@ var _validTypes = {
         array: true
     };
 
-function _isNumber(num) {
-    console.log(num);
-    if (typeof num === 'number') {
-        if (parseInt(num)+'' === num+'') {
-            console.log(parseInt(num)+'');
-            console.log(num);
-            return 'int';
-        } else if (parseFloat(num)+'' === num+'') {
-            console.log(parseFloat(num)+'');
-            console.log(num);
-            return 'float';
-        } else {
-            return 'boo';
+function _objName(obj) {
+    if (typeof obj === 'object') {
+        return (obj.constructor.name !== 'Object') ? '.'+obj.constructor.name : '';
+    }
+    return false;
+}
+
+function _isInt(num) {
+    if ((new String(num)).indexOf('.') < 0) {
+        if (parseInt(num) + '' === num + '') {
+            return 'number.int';
         }
     }
     return false;
 }
 
-var _type = {
-    'array': function (ob) { return (typeof ob.length === 'number' && typeof ob.push === 'function' ) },
-    object: function (ob) { return typeof ob === 'object'; },
-    number: _isNumber,
-    boolean: function (b) { return b === true || b === false},
-    'null': function (n) { return n === null; },
-    string: function (s) { return typeof s === 'string'; }
-};
+function _isFloat(num) {
+    if (parseFloat(num) + '' === num + '') {
+        if ((new String(num)).toLocaleLowerCase() !== (new String(num)).toLocaleUpperCase()) {
+            return 'number.scientific';
+        }
+        return 'number.float';
+    }
+    return false;
+}
 
+var _type = {
+    'array': function (ob) { return (typeof ob.length === 'number' && typeof ob.push === 'function' ) ? 'array' : false },
+    object: function (ob) { return typeof ob === 'object' ? 'object'+_objName(ob) : false; },
+    int: _isInt,
+    float: _isFloat,
+    boolean: function (b) { return b === true || b === false ? 'boolean' : false; },
+    'null': function (n) { return n === null ? 'null' : false; },
+    string: function (s) { return typeof s === 'string' ? 'string' : false; }
+};
 
 function getTypeDetails(inspect) {
     for (var t in _type) {
-        if (_type[t](inspect) !== false) return t;
+        _t = _type[t](inspect);
+        if (_t !== false) return _t;
     }
-    return false; // error a type has slipped
+    return typeof inspect; // error a type has slipped
 }
 
 module.exports = {
--- tests/specs/jacin-type-spec.js
+++ tests/specs/jacin-type-spec.js
@@ -3,11 +3,17 @@ require('./../../src/tool/test-signature')(__filename);
 var jacin = require('./../../index'),
     obj = jacin({});
 
+function CustomObject() {
+
+}
+
 describe('reads types', function () {
     it('basic ctypes', function () {
-        expect(obj.type().is(123)).toBe('int');
-        expect(obj.type().is(2.71828)).toBe('float');
+        expect(obj.type().is(123)).toBe('number.int');
+        expect(obj.type().is(2.71828)).toBe('number.float');
+        expect(obj.type().is(6.022E23)).toBe('number.scientific');
         expect(obj.type().is({})).toBe('object');
+        expect(obj.type().is(new CustomObject())).toBe('object.CustomObject');
         expect(obj.type().is([])).toBe('array');
     });
 });
